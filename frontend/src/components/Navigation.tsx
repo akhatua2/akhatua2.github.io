42bb0eaac7dc07c76106136aba7b14abcf92d183
Arpandeep Khatua <arpandeepk@gmail.com>
2025-01-13 04:17:46 -0800

    add

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -3,38 +3,68 @@
 import { motion } from "framer-motion"
 import Link from "next/link"
 import { useActiveSectionContext } from "@/context/active-section-context"
-import { links, SectionName } from "@/lib/data"
+import type { SectionName } from "@/context/active-section-context"
+
+const links = [
+  {
+    name: "About",
+    hash: "#About",
+  },
+  {
+    name: "Experience",
+    hash: "#Experience",
+  },
+  {
+    name: "Education",
+    hash: "#Education",
+  },
+  {
+    name: "Publications",
+    hash: "#Publications",
+  },
+  {
+    name: "Projects",
+    hash: "#Projects",
+  },
+  {
+    name: "Hobbies",
+    hash: "#Hobbies",
+  },
+  {
+    name: "Contact",
+    hash: "#Contact",
+  },
+] as const
 
 export default function Navigation() {
-  const { activeSection, setActiveSection, setTimeOfLastClick } =
-    useActiveSectionContext()
+  const { activeSection, setActiveSection, setTimeOfLastClick } = useActiveSectionContext()
 
   return (
     <header className="z-[999] relative">
       <motion.div
-        className="fixed top-0 left-1/2 -translate-x-1/2 h-[5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[4.5rem] sm:w-[50rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+        className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
 
-      <nav className="flex fixed top-[0.15rem] left-1/2 h-20 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[1.1rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-8">
-          {links.map((link: { name: SectionName; hash: string }) => (
+      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+          {links.map((link) => (
             <motion.li
-              className="relative h-3/4 flex items-center justify-center"
+              className="h-3/4 flex items-center justify-center relative"
               key={link.hash}
               initial={{ y: -100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
             >
               <Link
-                className={`flex w-full items-center justify-center px-7 py-4 hover:text-gray-950 transition dark:text-gray-400 dark:hover:text-gray-200 ${
+                className={`flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300 ${
                   activeSection === link.name
                     ? "text-gray-950 dark:text-gray-200"
                     : ""
                 }`}
                 href={link.hash}
                 onClick={() => {
-                  setActiveSection(link.name)
+                  setActiveSection(link.name as SectionName)
                   setTimeOfLastClick(Date.now())
                 }}
               >
