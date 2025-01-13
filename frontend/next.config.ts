9597b714d24b945cc20edf076678ed9d852665d7
Arpandeep Khatua <arpandeepk@gmail.com>
2025-01-13 04:08:29 -0800

    add

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/personal_website' : '',
+  basePath: '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/personal_website' : '',
   images: {
     unoptimized: true,
   },
